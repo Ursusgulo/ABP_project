fe6313f26602a2a48b4434edddcbabd2916b55b8
Ebba <ebba.hallen@gmail.com>
2026-06-22 16:03:24 +0200

    test

--- src/main.cpp
+++ src/main.cpp
@@ -89,14 +89,7 @@ void benchmark_spmv(const unsigned long N, const long long repeat, int gpu)
         .count();
   timings->h2d_s += host_to_dev_time;
 
-
-  cublasHandle_t handle;
-  cublasCreate(&handle);
   int n_blocks = (new_N + block_size - 1) / (block_size);
-  float axpy_scalar = -1;
-
-  new_vector_v<<<n_blocks, block_size>>>(new_N, d_w, beta, d_v, 0); // remove?
-
 
   // Measure time for spmv
   const unsigned long long int n_repeat =
@@ -117,12 +110,12 @@ void benchmark_spmv(const unsigned long N, const long long repeat, int gpu)
   
   float spmv_avg_s = time / n_repeat;
   float nnz_A = A.nnz;
-  float bytes_per_spmv = nnz_A( 2 * sizeof(float) + sizeof(int)) + N*N*N(sizeof(float) + 2 * sizeof(int)); //new
-  float gbytes = 1.0e-9;
+  // float bytes_per_spmv = nnz_A( 2 * sizeof(float) + sizeof(int)) + N*N*N(sizeof(float) + 2 * sizeof(int)); //new
+  float gbytes = 1.0e-9 * sizeof(float);
   float flops_per_spmv = nnz_A * 2; // 2 operations (mul + add) per non-zero
-  // float memops_per_spmv = nnz_A * 3 + 3 * N*N*N; // 3 memory ops (read val, read col, write res) per non-zero read
+  float memops_per_spmv = nnz_A * 3 + 3 * N*N*N; // 3 memory ops (read val, read col, write res) per non-zero read
   float gflops = flops_per_spmv * 1.0e-9 / spmv_avg_s; // 7 flops per non-zero
-  float bandwidth = bytes_per_spmv * gbytes / spmv_avg_s; // in GB/s
+  float bandwidth = memops_per_spmv * gbytes / spmv_avg_s; // in GB/s
 
   if(gpu)std::cout << N << ", " << m << ", " << gflops << ", " << bandwidth <<", " << h2d_avg_s << "\n";
   else std::cout << N << ", " << m << ", " << gflops << ", " << bandwidth <<"\n";
