fdee521f08276de5a0e1ef5bbe2ea7616db54daf
Ebba <ebba.hallen@gmail.com>
2026-06-22 16:16:55 +0200

    hej

--- src/lancoz.cuh
+++ src/lancoz.cuh
@@ -22,8 +22,8 @@ const int block_size = 512;
 
 
 void lancoz_gpu(const int N, const int m, SparseMatrixCRS<float>* result, Timings* timings);
-void d_compute_spmv(const std::size_t N,
-                             const std::size_t *row_starts,
+void d_compute_spmv(const int N,
+                             const int *row_starts,
                              const int *column_indices,
                              const float *values,
                              const float *x, 
